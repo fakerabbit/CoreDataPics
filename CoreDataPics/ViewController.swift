2895e34fceb3eb03bb7b6bc81840603b2130db30
Mirko Justiniano <mirkoj@gmail.com>
2020-08-13 10:28:24 -0500

    Initial project setup without storyboards

---
+++ CoreDataPics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataPics
+//
+//  Created by Mirko Justiniano on 8/13/20.
+//  Copyright © 2020 idevcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
